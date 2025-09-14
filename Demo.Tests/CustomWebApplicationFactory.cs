4e00bae04738a5e857f32ee0ea6e36dfdd4f0b76
Aayden Goh <gwy151095@gmail.com>
2025-09-14 11:55:23 +0800

    update with extra example and add region

--- Demo.Tests/CustomWebApplicationFactory.cs
+++ Demo.Tests/CustomWebApplicationFactory.cs
@@ -18,13 +18,22 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
         _executionLog.Add("ConfigureWebHost Start");
         builder.ConfigureTestServices(services =>
         {
-            _executionLog.Add("ConfigureTestServices Start");            
+            _executionLog.Add("ConfigureTestServices Start");
+            #region Common Service Registration
+            //When you have multiple registrations for the same service type, the last registration wins            
+            services.AddSingleton<IConfigurationService, MockConfigurationService>();
+            //or
+            //This would explicitly replace the existing registration
             services.Replace(new ServiceDescriptor(typeof(IConfigurationService), typeof(MockConfigurationService), ServiceLifetime.Singleton));
+            #endregion
+
+            #region Partial Service Modification
             services.Replace<DbService>(options =>
             {
                 options._databaseConnection = "test-postgresql-connection";
                 return options;
             });
+            #endregion
             _executionLog.Add("ConfigureTestServices End");
         });
 
